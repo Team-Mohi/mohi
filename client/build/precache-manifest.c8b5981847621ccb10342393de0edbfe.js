ee1e34538fc01d983bcd31bb9471742c90e00682
muoingon <phuocnv106@gmail.com>
2020-09-16 00:39:36 +0700

    nguyenvanphuoc

---
+++ client/build/precache-manifest.c8b5981847621ccb10342393de0edbfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791705ef27cbac4ba0ba8311e7218e2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f22a56659b5457af36",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf1396226e64f9cf8d45",
+    "url": "/static/js/2.a397b7aa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a397b7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f22a56659b5457af36",
+    "url": "/static/js/main.09343add.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
